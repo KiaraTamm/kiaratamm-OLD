fc80bdfd710da747a8d1ec0cd54bcf0740bdeaca
Kiara Tamm <kiaratamm@vistomail.com>
2014-12-01 09:15:02 -0500

    Automated report publishing - 2014-12-01 14:15:02.016500

--- data/routes.js
+++ data/routes.js
@@ -7,3 +7,5 @@ var writePubTime = "2014-12-01 13:30:02.088917";
 var writePubTime = "2014-12-01 13:45:01.879214";
 
 var writePubTime = "2014-12-01 14:00:01.941889";
+
+var writePubTime = "2014-12-01 14:15:02.016500";
