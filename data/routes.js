d031b0612588948ee75d2b264d4f842d3af7b563
Kiara Tamm <kiaratamm@vistomail.com>
2014-12-01 09:45:01 -0500

    Automated report publishing - 2014-12-01 14:45:01.819512

--- data/routes.js
+++ data/routes.js
@@ -1,3 +1,5 @@
 var bots = [ "../data/ccedk_nbtusd", "../data/ccedk_nbtbtc", "../data/ccedk_nbteur", "../data/ccedk_nbtppc" ];
 bots.sort();
 var writePubTime = "2014-12-01 14:43:42.082059";
+
+var writePubTime = "2014-12-01 14:45:01.819512";
