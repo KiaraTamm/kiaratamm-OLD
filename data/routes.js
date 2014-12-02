ebae37301d8c01c664ef55c141d7a00b4facf5c0
Kiara Tamm <kiaratamm@vistomail.com>
2014-12-02 15:06:29 -0500

    Automated report publishing - 2014-12-02 20:06:29.783813

--- data/routes.js
+++ data/routes.js
@@ -1,31 +1,3 @@
-var bots = [ "../data/ccedk_nbtusd", "../data/ccedk_nbtbtc", "../data/ccedk_nbteur", "../data/ccedk_nbtppc" ];
+var bots = [ "../data/ccedk_nbtppc", "../data/ccedk_nbtbtc", "../data/ccedk_nbteur", "../data/ccedk_nbtusd" ];
 bots.sort();
-var writePubTime = "2014-12-02 16:39:01.076593";
-
-var writePubTime = "2014-12-02 16:45:02.235478";
-
-var writePubTime = "2014-12-02 17:00:02.425222";
-
-var writePubTime = "2014-12-02 17:15:01.627800";
-
-var writePubTime = "2014-12-02 17:30:02.220395";
-
-var writePubTime = "2014-12-02 17:45:01.480508";
-
-var writePubTime = "2014-12-02 18:00:01.564065";
-
-var writePubTime = "2014-12-02 18:15:02.347111";
-
-var writePubTime = "2014-12-02 18:30:02.633881";
-
-var writePubTime = "2014-12-02 18:45:02.510861";
-
-var writePubTime = "2014-12-02 19:00:01.669859";
-
-var writePubTime = "2014-12-02 19:15:01.842046";
-
-var writePubTime = "2014-12-02 19:30:01.813016";
-
-var writePubTime = "2014-12-02 19:45:02.290190";
-
-var writePubTime = "2014-12-02 20:00:01.832903";
+var writePubTime = "2014-12-02 20:06:29.783813";
